18bca5c92c674fa10089537e217805d5714244c7
Prashant-AK <ps.prashantsingh.98@gmail.com>
2022-01-09 15:40:28 +0530

    changes for auth

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -3,10 +3,8 @@ const jwt = require("jsonwebtoken");
 const config = process.env;
 
 const verifyToken = (req, res, next) => {
-  console.log('token : ',req.headers,req.query.token);
   const token =
-    req.body.token || req.query.token || req.headers["access-token"];
-
+    req.body.token || req.query.token || req.headers.authorization.split("Bearer ")[1];
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
